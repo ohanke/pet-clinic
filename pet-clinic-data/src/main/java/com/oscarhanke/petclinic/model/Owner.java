571811bdc1d8ee7136018287c5de5e55756be240
ohanke <steamoha@wp.pl>
2022-05-05 16:51:11 +0200

    Updated Owner with JPA Entity Mapping. Closes #37.

--- pet-clinic-data/src/main/java/com/oscarhanke/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/oscarhanke/petclinic/model/Owner.java
@@ -1,13 +1,27 @@
 package com.oscarhanke.petclinic.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @Column(name = "adress")
     private String adress;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAdress() {
