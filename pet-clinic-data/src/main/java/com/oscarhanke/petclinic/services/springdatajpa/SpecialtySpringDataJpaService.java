a53df9259bef04bdc621c5df7fa294f0772d02eb
ohanke <steamoha@wp.pl>
2022-05-06 13:14:56 +0200

    Added Spring Data JPA Service for Specialties. Fixes Names of Some Map Services. Closes #44.

---
+++ pet-clinic-data/src/main/java/com/oscarhanke/petclinic/services/springdatajpa/SpecialtySpringDataJpaService.java
@@ -0,0 +1,48 @@
+package com.oscarhanke.petclinic.services.springdatajpa;
+
+import com.oscarhanke.petclinic.model.Specialty;
+import com.oscarhanke.petclinic.services.SpecialtyService;
+import com.oscarhanke.repositories.SpecialtyRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySpringDataJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySpringDataJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long Id) {
+        return specialtyRepository.findById(Id).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return specialtyRepository.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        specialtyRepository.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long Id) {
+        specialtyRepository.deleteById(Id);
+    }
+}
